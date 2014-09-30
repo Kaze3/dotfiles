b575738597aedccac6e8e8603598432f3bf052d2
Henry HTPC <henry@henryhtpc.homeip.net>
2014-09-30 22:01:13 +0100

    Change emacs config to .emacs.d/init.el

--- .emacs
+++
@@ -1,8 +0,0 @@
-; replace tabs with spaces and set default width to 2
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
-(setq tab-stop-list (number-sequence 2 200 2))
-
-; line numbers
-(global-linum-mode t)
-(setq linum-format "%d ")
---
+++ .emacs.d/init.el
@@ -0,0 +1,22 @@
+(add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized/")
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list 'package-archives
+	       '("marmalade" . "http://marmalade-repo.org/packages/") t)
+  (add-to-list 'package-archives
+  	       '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (package-initialize)
+
+  (load-theme 'solarized-dark t)
+)
+
+(setq backup-by-copying t)
+(setq backup-directory-alist '(("." . "~/.emacs_saves")))
+
+(require 'auto-complete-config)
+(ac-config-default)
+
+(require 'yasnippet)
+(yas-global-mode 1)
+
